dce0e1dc3fc8644d8a7d6170837af75920a39bfd
Stray <cagri.c.tasci@outlook.de>
2017-11-13 15:58:23 +0100

    Setup basic View-structure for AvatarViewFragment

--- app/src/main/java/com/strayswonderland/avatarcreatortool/Fragments/AvatarViewFragment.java
+++ app/src/main/java/com/strayswonderland/avatarcreatortool/Fragments/AvatarViewFragment.java
@@ -5,10 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.strayswonderland.avatarcreatortool.R;
 
 public class AvatarViewFragment extends Fragment {
+    ImageView avatarPreview;
+    ImageButton contextMenueButton;
+
     public AvatarViewFragment() {
         // Required empty public constructor
     }
@@ -21,8 +26,13 @@ public class AvatarViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_avatar_view, container, false);
+
+        avatarPreview = (ImageView) view.findViewById(R.id.iv_avatarView);
+        contextMenueButton = (ImageButton) view.findViewById(R.id.ib_contextMenue);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_avatar_view, container, false);
+        return view;
     }
 
 }
