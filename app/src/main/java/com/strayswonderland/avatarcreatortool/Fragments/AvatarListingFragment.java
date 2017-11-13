11936aa8bcfa33a1441ef77a4af72060f5876bd0
Stray <cagri.c.tasci@outlook.de>
2017-11-13 16:21:57 +0100

    Setup basic View-structure for  ListFragment and SettingsFragment

--- app/src/main/java/com/strayswonderland/avatarcreatortool/Fragments/AvatarListingFragment.java
+++ app/src/main/java/com/strayswonderland/avatarcreatortool/Fragments/AvatarListingFragment.java
@@ -21,8 +21,10 @@ public class AvatarListingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_avatar_listing, container, false);
+        View view = inflater.inflate(R.layout.fragment_avatar_listing, container, false);
+
+
+        return view;
     }
 
 }
