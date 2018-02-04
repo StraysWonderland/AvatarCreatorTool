5698b421c0044faf0e211138f4feb45e72b1ce13
Stray <st111605@stud.uni-stuttgart.de>
2018-02-04 16:52:03 +0100

    find all buttons in EditAvatarFragment

--- app/src/main/java/com/strayswonderland/avatarcreatortool/Fragments/EditAvatarFragment.kt
+++ app/src/main/java/com/strayswonderland/avatarcreatortool/Fragments/EditAvatarFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageSwitcher
 
 import com.strayswonderland.avatarcreatortool.R
 
@@ -19,12 +21,24 @@ class EditAvatarFragment : Fragment() {
         /*
         *   TODO: find and bind following:
         *   Section buttons
-        *   Confirm Button
         *   Cancel button
         *   Swiping to Randomize??
          */
 
         val confirmButton = view.findViewById(R.id.bt_confirmAvatar) as Button
+        confirmButton.setOnClickListener{
+            // TODO; save Avatar with current features in Database
+            // destroy current Fragment
+        }
+
+        val headSectionButton = view.findViewById(R.id.bt_HeadFeatureSelection) as ImageButton
+        val skinColorSectionButton = view.findViewById(R.id.bt_SkinColorFeatureSelection) as ImageButton
+        val eyesSectionButton = view.findViewById(R.id.bt_EyesFeatureSelection) as ImageButton
+        val eyeColorSectionButton = view.findViewById(R.id.bt_EyeColorFeatureSelection) as ImageButton
+        val mouthSectionButton = view.findViewById(R.id.bt_MouthFeatureSelection) as ImageButton
+        val hairSectionButton = view.findViewById(R.id.bt_HairFeatureSelection) as ImageButton
+        val hairColorSectionButton = view.findViewById(R.id.bt_HairColorFeatureSelection) as ImageButton
+        val noseSectionButton = view.findViewById(R.id.bt_NoseFeatureSelection) as ImageButton
 
         return view
     }
